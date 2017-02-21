bacaae0ce55655f1c04162e274acffc7e6296ac8
Joanne <jeffiefeifei@gmail.com>
2017-02-20 17:39:37 -0800

    [set] webpack react environment

---
+++ src/client/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import HomePage from './components/HomePage.jsx';
+import AwesomeComponent from './components/AwesomeComponent.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <HomePage />
+        <AwesomeComponent />
+      </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
